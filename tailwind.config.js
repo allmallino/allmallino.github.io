7e300bd624be5a5b451a03ca6ca231c14b8df8d6
allmall <zavarzin03@gmail.com>
2023-11-09 22:33:09 +0200

    Header is done, sidebar is nearly done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'gray': '#f0f0f0',
+        'transparentgray':'#0000001A',
+        'halfgray':'#00000099',
+        'green':'#00C12BFF',
+        'red':'#F50606',
+        'yellow':'#F5DD06',
+        'orange':'#F57906',
+        'azure':'#06CAF5',
+        'blue':'#063AF5',
+        'purple':'#7D06F5',
+        'pink':'#F506A4',
+      },
+    },
   },
   plugins: [],
 }
