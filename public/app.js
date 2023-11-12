85fc2e1376d82bbacd6d5cd0938f4e3532c603ad
allmall <zavarzin03@gmail.com>
2023-11-12 17:58:33 +0200

    Added popup, recommendations api and minor stuff

--- public/app.js
+++ public/app.js
@@ -1,75 +1,209 @@
 async function getProducts(page = 1) {
     let limit =window.innerWidth>=1280?9:6;
-    var products = await fetch('https://voodoo-sandbox.myshopify.com/products.json?limit='+limit+"&page="+page)
+    let products = await fetch('https://voodoo-sandbox.myshopify.com/products.json?limit='+limit+"&page="+page)
     .then(response => response.json())
     .then(data => { return data.products });
-    console.log(products);
-
+    
     let table = document.getElementById("table");
     table.innerHTML="";
     let fragment = document.createDocumentFragment();
     for(let i=0; i<products.length; i++){
-        let product = products[i];
-        let article = document.createElement("article");
-        article.classList.add("w-44", "xl:w-72");
-
-        let img = document.createElement("img");
-        img.src=product.images.length>=1?product.images[0].src:"./images/shopify_image.png";
-        img.classList.add("w-44", "h-44", "xl:w-72", "xl:h-72", "rounded-3xl", "mb-4", "object-cover");
-        article.appendChild(img);
-
-        let h4 = document.createElement("h4");
-        h4.textContent=product.title;
-        h4.classList.add("xl:text-xl","font-bold", "mb-2.5", "truncate", "w-full");
-        article.appendChild(h4);
-
-        let conteiner = document.createElement("div");
-        conteiner.classList.add("flex", "gap-3", "mb-2.5");
-
-        let stars = document.createElement("div");
-        stars.classList.add("flex", "gap-1");
-
-        for(let j=0; j<3;j++){
-            let star = document.createElement("img");
-            star.src="./svg/full_star_icon.svg";
-            stars.appendChild(star);
-        }
-
-        let half_star= document.createElement("img");
-        half_star.src="./svg/half_star_icon.svg";
-        stars.appendChild(half_star);
-        conteiner.appendChild(stars);
-
-        let rate = document.createElement("span");
-        rate.textContent="3.5/";
-        rate.classList.add("text-xs", "xl:text-sm");
-
-        let full = document.createElement("span");
-        full.textContent="5"
-        full.classList.add("text-halfgray");
-        rate.appendChild(full);
-        conteiner.appendChild(rate);
-        article.appendChild(conteiner);
-
-        let cost = document.createElement('h3');
-        cost.textContent="$"+product.variants[0].price.replace(".00","");
-        cost.classList.add("text-xl", "xl:text-2xl", "font-bold")
-        article.appendChild(cost);
-        fragment.appendChild(article);
+        fragment.appendChild(createProductCard(products[i]));
     }
     table.appendChild(fragment);
     renderPage(page);
 }
 
+function createProductPopUp(product, options){
+  let selectedProduct=product.variants.filter((v)=>{
+    for(let i=0 ; i<options.length; i++){
+      if(v["option"+(i+1)]!==product.options[i].values[options[i]]){
+        return false;
+      }
+    }
+    return true;
+  })[0];
+  let variants = document.getElementById('variants')
+  variants.innerHTML="";
+  let div, name, optionList, option, line;
+  let fragment = document.createDocumentFragment();
+  for(let i=0;i<product.options.length;i++){
+    div=document.createElement('div');
+    div.className="text-halfgray";
+    name=document.createElement('span');
+    name.className="text-halfgray text-sm xl:text-base";
+    name.textContent="Select "+product.options[i].name;
+    div.appendChild(name);
+    optionList=document.createElement('div');
+    optionList.className="text-halfgray";
+    if(product.options[i].name==="Color"){
+      optionList.className="flex gap-3 xl:gap-4 mt-4 overflow-x-auto w-full";
+      for(let j=0; j<product.options[i].values.length;j++){
+        option=document.createElement('div');
+        option.className="border border-transparentgray rounded-full inline-block w-9 h-9 flex items-center justify-center cursor-pointer flex-shrink-0 bg-"+product.options[i].values[j].toLowerCase()
+        if(j===options[i]){
+          let check = document.createElement("img");
+          if(product.options[i].values[j].toLowerCase()==="white"){
+            check.className="bg-halfgray rounded-full p-1";
+          }
+          check.src="./svg/check_icon.svg";
+          option.appendChild(check);
+        }
+        option.onclick=()=>{options[i]=j;createProductPopUp(product, options)};
+        optionList.appendChild(option);
+      }
+    }else{
+      optionList.className="flex gap-2 xl:gap-3 flex-wrap mt-4 text-sm xl:text-base xl:leading-5 overflow-x-auto w-full"
+      for(let j=0; j<product.options[i].values.length;j++){
+        option=document.createElement('span');
+        let selected = j===options[i]?"bg-black text-white":"bg-gray";
+        option.className="py-2.5 xl:py-3 px-5 xl:px-6 rounded-full cursor-pointer flex-shrink-0 "+selected;
+        option.textContent=product.options[i].values[j];
+        option.onclick=()=>{options[i]=j;createProductPopUp(product, options)};
+        optionList.appendChild(option);
+      }
+    }
+    div.appendChild(optionList);
+    fragment.appendChild(div);
+    line = document.createElement('hr')
+    line.className="text-transparentgray my-6";
+    fragment.appendChild(line);
+  }
+  variants.appendChild(fragment);
+  if(typeof selectedProduct == 'undefined'){
+    document.getElementById("buybutton").textContent="Not available";
+    document.getElementById("buybutton").disabled = true;
+    return;
+  }else if(!selectedProduct.available){
+    document.getElementById("buybutton").textContent="Not available";
+    document.getElementById("buybutton").disabled = true;
+  }else{
+    document.getElementById("buybutton").textContent="Add to Cart";
+    document.getElementById("buybutton").disabled = false;
+  }
+
+  document.getElementById('pop-up').classList.replace('hidden', "flex");
+  document.getElementById('image').src=selectedProduct.featured_image?selectedProduct.featured_image.src:"./images/shopify_image.png";
+  document.getElementById('name').textContent=product.title;
+  document.getElementById('price').textContent="$"+selectedProduct.price.toString().replace(".00", "");
+  if(selectedProduct.compare_at_price !== null && parseFloat(selectedProduct.compare_at_price)>parseFloat(selectedProduct.price)){
+    document.getElementById('oldprice').classList.replace('hidden', "flex");
+    document.getElementById('oldprice').textContent="$"+selectedProduct.compare_at_price.toString().replace(".00", "");
+
+    document.getElementById('discount').classList.replace('hidden', "flex");
+    document.getElementById('discount').textContent="-"+(100-Math.floor(parseFloat(selectedProduct.price)/parseFloat(selectedProduct.compare_at_price)*100))+"%";
+  }else{
+    document.getElementById('oldprice').classList.replace('flex', "hidden");
+    document.getElementById('discount').classList.replace('flex', "hidden");
+  }
+  let rating = document.getElementById('rating');
+  rating.innerHTML="";
+  let stars = document.createElement("div");
+  stars.classList.add("flex", "gap-2");
+  for(let j=0; j<3;j++){
+      let star = document.createElement("img");
+      star.src="./svg/full_star_icon.svg";
+      stars.appendChild(star);
+  }
+  let half_star= document.createElement("img");
+  half_star.src="./svg/half_star_icon.svg";
+  stars.appendChild(half_star);
+  rating.appendChild(stars);
+  let rate = document.createElement("span");
+  rate.textContent="3.5/";
+  let full = document.createElement("span");
+  full.textContent="5"
+  full.classList.add("text-halfgray");
+  rate.appendChild(full);
+  rating.appendChild(rate);
+  document.getElementById('description').innerHTML=product.body_html?product.body_html:product.description?product.description:"";
+  return true;
+}
+
+function createProductCard(product, ...classList){
+  let article = document.createElement("article");
+  article.classList.add("w-44", "xl:w-72", "cursor-pointer", ...classList);
+  article.onclick= async () => {
+    createProductPopUp(product, product.options.map(()=>{return 0}));
+    let recommendation = document.getElementById('recommendation');
+    recommendation.innerHTML="";
+    let recList= await fetch('https://voodoo-dev-store.com/recommendations/products.json?product_id='+product.id)
+    .then(response => response.json())
+    .then(data => { return data.products });
+    fragment = document.createDocumentFragment();
+    for(let i = 0; i<recList.length; i++){
+      fragment.appendChild(createProductCard(recList[i],"flex-shrink-0"));
+    }
+    recommendation.appendChild(fragment);
+  }
+
+  let img = document.createElement("img");
+  img.src=product.featured_image?product.featured_image:product.images[0]?product.images[0].src:"./images/shopify_image.png";
+  img.classList.add("w-44", "h-44", "xl:w-72", "xl:h-72", "rounded-3xl", "mb-4", "object-cover");
+  article.appendChild(img);
+
+  let h4 = document.createElement("h4");
+  h4.textContent=product.title;
+  h4.classList.add("xl:text-xl","font-bold", "mb-2.5", "truncate", "w-full");
+  article.appendChild(h4);
+
+  let conteiner = document.createElement("div");
+  conteiner.classList.add("flex", "gap-3", "mb-2.5");
+
+  let stars = document.createElement("div");
+  stars.classList.add("flex", "gap-1");
+
+  for(let j=0; j<3;j++){
+      let star = document.createElement("img");
+      star.src="./svg/full_star_icon.svg";
+      stars.appendChild(star);
+  }
+
+  let half_star= document.createElement("img");
+  half_star.src="./svg/half_star_icon.svg";
+  stars.appendChild(half_star);
+  conteiner.appendChild(stars);
+
+  let rate = document.createElement("span");
+  rate.textContent="3.5/";
+  rate.classList.add("text-xs", "xl:text-sm");
+
+  let full = document.createElement("span");
+  full.textContent="5"
+  full.classList.add("text-halfgray");
+  rate.appendChild(full);
+  conteiner.appendChild(rate);
+  article.appendChild(conteiner);
+  
+  let oldPrice=product.compare_at_price?product.compare_at_price:product.variants[0].compare_at_price;
+  let newPrice=product.price?product.price:product.variants[0].price;
+  let div = document.createElement('div');
+  div.classList.add("text-xl", "xl:text-2xl", "font-bold", "flex", "gap-1", "xl:gap-2.5", "items-center", "flex-wrap");
+  let nCost = document.createElement('h3');
+  nCost.textContent="$"+newPrice.toString().replace(".00", "");
+  div.appendChild(nCost);
+  if(oldPrice !== null && parseFloat(oldPrice)>parseFloat(newPrice)){
+    let oCost = document.createElement('h3');
+    oCost.textContent="$"+oldPrice.toString().replace(".00", "");
+    oCost.className="text-gray40% line-through decoration-2";
+    div.appendChild(oCost);
+
+    let discount = document.createElement('span');
+    discount.textContent="-"+(100-Math.floor(parseFloat(newPrice)/parseFloat(oldPrice)*100))+"%";
+    discount.className="bg-lightred text-textred px-3.5 py-1.5 rounded-full xl:leading-5 font-medium text-xxs xl:text-xs";
+    div.appendChild(discount);
+  }
+  article.appendChild(div);
+  return article;
+}
 
 function createTableLi(cf, options) {
   let li = document.createElement('li');
-  let a = document.createElement('a');
-  a.href = "#label";
-  a.className = options.cl;
-  a.text = options.txt;
-  a.onclick = cf;
-  li.append(a);
+  let span = document.createElement('span');
+  span.className = options.cl;
+  span.textContent = options.txt;
+  span.onclick = cf;
+  li.append(span);
   return li;
 }
 
@@ -84,7 +218,7 @@ function renderPage(index=1){
   let points;
   if(index == count || index + 1 == count){
     for (let i = 1; i <= 2+Math.floor(limit/9); i++) {
-      fragmet.append(createTableLi(() => getProducts(i), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9 ", txt: i }));
+      fragmet.append(createTableLi(() => getProducts(i), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9 cursor-pointer", txt: i }));
     }
     points = document.createElement('li');
     points.textContent = "...";
@@ -92,29 +226,29 @@ function renderPage(index=1){
     fragmet.append(points);
     
     for (let i = count-1-Math.floor(limit/9); i <= count; i++) {
-      let selected = i === index  ? "text-black bg-lightblack" : "";
+      let selected = i === index  ? "text-black bg-lightblack" : "cursor-pointer";
       fragmet.append(createTableLi(() => getProducts(i), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9 "+ selected, txt: i}));
     }
   }
   else if (index > 2) {
-    fragmet.append(createTableLi(() => getProducts(1), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9", txt: "1" }));
+    fragmet.append(createTableLi(() => getProducts(1), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9 cursor-pointer", txt: "1" }));
     points = document.createElement('li');
     points.textContent = "...";
     points.className="rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9";
     fragmet.append(points);
     for (let i = -Math.floor(limit/9); i <= Math.floor(limit/9) && i + index <= count; i++) {
-      let selected = i === 0  ? "text-black bg-lightblack" : "";
+      let selected = i === 0  ? "text-black bg-lightblack" : "cursor-pointer";
       fragmet.append(createTableLi(() => getProducts(i + index), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9 "+ selected, txt: i + index }));
     }
     points = document.createElement('li');
     points.textContent = "...";
     points.className="rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9";
     fragmet.append(points);
-    fragmet.append(createTableLi(() => getProducts(count), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9", txt: count }));
+    fragmet.append(createTableLi(() => getProducts(count), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9 cursor-pointer", txt: count }));
   }
   else {
       for (let i = 1; i <= 2+Math.floor(limit/9); i++) {
-        let selected = i === index ? "text-black bg-lightblack" : "";
+        let selected = i === index ? "text-black bg-lightblack" : "cursor-pointer";
         fragmet.append(createTableLi(() => getProducts(i), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9 "+ selected, txt: i }));
       }
       
@@ -124,7 +258,7 @@ function renderPage(index=1){
       fragmet.append(points);
 
       for (let i = count-1-Math.floor(limit/9); i <= count; i++) {
-        fragmet.append(createTableLi(() => getProducts(i), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9", txt: i }));
+        fragmet.append(createTableLi(() => getProducts(i), { cl: "rounded-lg flex items-center justify-center xl:w-10 xl:h-10 w-9 h-9 cursor-pointer", txt: i }));
       }
   }
   pageList.append(fragmet);
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,23 +9,31 @@ module.exports = {
         'gray': '#f0f0f0',
         'transparentgray':'#0000001A',
         'halfgray':'#00000099',
+        'gray30%':'#0000004D',
+        'gray40%':'#00000066',
         'halfblack':'#00000080',
         'lightblack':'#0000000F',
         'green':'#00C12BFF',
         'red':'#F50606',
+        'textred':'#FF3333',
+        'lightred':'#FF33331A',
         'yellow':'#F5DD06',
         'orange':'#F57906',
         'azure':'#06CAF5',
         'blue':'#063AF5',
         'purple':'#7D06F5',
         'pink':'#F506A4',
+        'navy':'#000080',
+        'dark':'#3A3B3C'
       },
       fontFamily:{
         logo:['Avenir'],
         body:['Plus Jakarta Sans', 'sans-serif'],
+      },
+      fontSize: {
+        xxs: ['10px', '13px']
       }
     },
   },
   plugins: [],
 }
-
